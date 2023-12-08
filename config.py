eb6ad96f61ed610db5b4652fe90b4f71eb2b29b9
Paul Taylor <78972614+PTaylor18@users.noreply.github.com>
2023-12-08 15:51:22 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-config = {'bootstrap.servers': 'pkc-41wq6.eu-west-2.aws.confluent.cloud:9092',
+config = {'bootstrap.servers': 'BOOTSTRAP_SERVERS',
           'security.protocol': 'SASL_SSL',
           'sasl.mechanisms': 'PLAIN',
-          'sasl.username': 'XQSGLMUQSWETZ3IO',
-          'sasl.password': 'zZ6ZNz5l8U/AoHWaLnGbLg0oGphmE8ZwbkJ4Yqu8qsniL8YojPVlknDlC6+c5M5K'
+          'sasl.username': 'SASL_USERNAME',
+          'sasl.password': 'SASL_PASSWORD'
           }
 
 sr_config = {
-    'url': 'https://psrc-gn6wr.us-east-2.aws.confluent.cloud',
-    'basic.auth.user.info': 'S2NEMY5OLZKI3CH6:wrPbTPz48cvfhl4KMdtS8XQUGeT+VW6JLryw5XI2XsS5dFsgNO2bFkyVVhivDVao'
+    'url': 'URL',
+    'basic.auth.user.info': 'AUTH'
 }
