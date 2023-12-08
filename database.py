e874438bad583df18bf1418426444c282f50f43a
Paul Taylor <78972614+PTaylor18@users.noreply.github.com>
2023-12-08 15:51:52 +0000

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:password@orders-db.chxfodaqpbog.eu-west-2.rds.amazonaws.com:5432/orders"
+SQLALCHEMY_DATABASE_URL = "DB_URL"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
